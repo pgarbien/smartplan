3a246e6e628c87bbb63c6e24106f2d4b9ce65a0a
Piotr Garbień <piotr.garbien@gmail.com>
2020-04-13 22:11:47 +0200

    Added basic configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {HttpModule, Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import configuration from "../config/configuration";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        HttpModule,
+        ConfigModule.forRoot({
+            envFilePath: './config/api_config.env',
+            load: [configuration]
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+
+import configuration from "../config/configuration";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(configuration().port);
 }
+
+
 bootstrap();
