8471385fd7976b343aad127ac7e3072d83806b44
Eryk Sikora <eryk.sikora@msleopard.com>
2020-08-28 20:37:23 +0200

    Added motivational video
    Former-commit-id: 75fe9c90eaefff5ccb284637d6fcd9bfc762f4e1

--- frontend/src/containers/Locations/Locations.js
+++ frontend/src/containers/Locations/Locations.js
@@ -8,7 +8,7 @@ const Locations = () => {
     const history = useHistory();
 
     const locationsa = locations.map(location => {
-        return <div onClick={() => {routeChange(location.id)}} style={{borderRadius: 5, overflow: "hidden", width: "30%", display: "inline-block", margin: "0 3% 3% 0"}}>
+        return <div onClick={() => {routeChange(`draw/` + location.id)}} className="location" style={{borderRadius: 5, overflow: "hidden", width: "31%", display: "inline-block", marginBottom: "3%"}}>
             <div style={{backgroundImage: "url('https://www.homekoncept.com.pl/wp-content/uploads/2020/05/HomeKONCEPT-NH-717-zdjecie-1.jpg')", backgroundSize: "cover", backgroundPositionX: "50%", height: 200}}>
 
             </div>
@@ -18,8 +18,7 @@ const Locations = () => {
         </div>
     });
 
-    const routeChange = (locationId) =>{ 
-        let path = `draw/` + locationId; 
+    const routeChange = (path) =>{
         history.push(path);
     }
 
@@ -41,6 +40,14 @@ const Locations = () => {
         <div>
             <h2 style={{fontSize: "1.5em"}}>Your locations:</h2>
             { locations != [] ? locationsa : "aaa" }
+            <div onClick={() => {routeChange(`draw/`)}} className="location" style={{borderRadius: 5, overflow: "hidden", width: "31%", display: "inline-block", marginBottom: "3%"}}>
+                <div style={{backgroundImage: "url('https://static.thenounproject.com/png/2996504-200.png')", backgroundSize: "100px 100px", backgroundRepeat: "no-repeat", backgroundPosition: "50%", height: 200, borderWidth: "1px 1px 0px 1px", borderColor: "rgb(198, 198, 198)", borderStyle: "solid", borderRadius: "5px 5px 0px 0px"}}>
+
+                </div>
+                <div style={{textAlign: "center", padding: 15, fontSize: 20, borderWidth: "1px 1px 1px 1px", borderColor: "rgb(198, 198, 198)", borderStyle: "solid", borderRadius: "0px 0px 5px 5px", backgroundColor: "#fff"}}>
+                    Add new location
+                </div>
+            </div>
         </div>
     );
 }
