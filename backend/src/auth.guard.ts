0a3ee6cb7a4ca60b00f561bcf766ec572ba4f6db
Piotr Garbien <piotr.garbien@msleopard.com>
2020-10-03 23:32:29 +0200

    Changed response code from 403 to 401
    Former-commit-id: 88c7e6472ebc34378253af02f8ebf7c520bb45ac

--- backend/src/auth.guard.ts
+++ backend/src/auth.guard.ts
@@ -1,13 +1,18 @@
-import {CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
+import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from "@nestjs/common";
 import {Observable} from "rxjs";
 import AuthService from "./auth/auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {
+    }
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const request = context.switchToHttp().getRequest();
-        return this.authService.checkIfLoggedIn(request.headers['authorization']);
+        if (this.authService.checkIfLoggedIn(request.headers['authorization'])) {
+            return true;
+        } else {
+            throw new UnauthorizedException()
+        }
     }
 }
