193617059098f1fcb6fdbb2a1a0aca6517cd58f1
Piotr Garbien <piotr.garbien@msleopard.com>
2020-10-04 22:55:49 +0200

    Fixed deleting location bug after moving to mongo
    Former-commit-id: 4f27b055bee998d7004693978ebb77a21146207f

--- backend/src/locations/locations.service.ts
+++ backend/src/locations/locations.service.ts
@@ -1,37 +1,13 @@
-import {HttpService, Injectable} from '@nestjs/common';
-import {ConfigService} from "@nestjs/config";
+import {Injectable} from '@nestjs/common';
 import {Location} from "./location.model";
-import {Repository, UpdateResult} from "typeorm";
+import {MongoRepository, UpdateResult} from "typeorm";
 import {InjectRepository} from "@nestjs/typeorm";
 import {randomStringGenerator} from "@nestjs/common/utils/random-string-generator.util";
 
 @Injectable()
 export class LocationsService {
-    constructor(private readonly configService: ConfigService,
-                private readonly httpService: HttpService,
-                @InjectRepository(Location) private readonly locationsRepository: Repository<Location>) {
+    constructor(@InjectRepository(Location) private readonly locationsRepository: MongoRepository<Location>) {
     }
-
-    private apiKey: string = this.configService.get<string>('API_KEY');
-    private apiUrl = this.configService.get<string>('API_URL') + '/channels';
-
-    private config = {
-        headers: {Authorization: `Bearer ${this.apiKey}`}
-    };
-
-    // getLocations() {
-    //     return this.httpService.get(this.apiUrl, this.config)
-    //         .pipe(
-    //             map(response => response.data)
-    //         );
-    // }
-    //
-    // getLocationById(locationId: number) {
-    //     return this.httpService.get(this.apiUrl + `/${locationId}`)
-    //         .pipe(
-    //             map(response => response.data)
-    //         );
-    // }
 
     getAll(userId: string): Promise<Location[]> {
         return this.locationsRepository.find({"userId": userId});
@@ -51,14 +27,13 @@ export class LocationsService {
             level.rooms.forEach(room => {
                 room.userId = userId;
                 room.id = randomStringGenerator();
-                // room.points = JSON.stringify(room.points);
             })
         });
         return await this.locationsRepository.save(location);
     }
 
     deleteById(userId: string, locationId: string) {
-        this.locationsRepository.delete({
+        this.locationsRepository.deleteOne({
             "id": locationId,
             "userId": userId
         })
@@ -66,12 +41,12 @@ export class LocationsService {
 
     async update(userId: string, location: Location): Promise<UpdateResult> {
         location.levels.forEach(level => {
-            if(level.id  == null) {
+            if (level.id == null) {
                 level.userId = userId;
                 level.id = randomStringGenerator();
             }
             level.rooms.forEach(room => {
-                if(room.id == null) {
+                if (room.id == null) {
                     room.userId = userId;
                     room.id = randomStringGenerator();
                 }
