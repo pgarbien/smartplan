8d9eced16348e0fe3805e96cfb2e79866160bc18
Katarzyna Strzyż <kstrzyz@student.agh.edu.pl>
2020-11-12 23:20:15 +0100

    Change css to main page
    Former-commit-id: 3395a70930537984d764f2b9a20b4708b6f33a6e

--- frontend/src/containers/Layout/Layout.js
+++ frontend/src/containers/Layout/Layout.js
@@ -1,13 +1,28 @@
 import React, { Fragment } from 'react';
 import Toolbar from '../../components/Toolbar/Toolbar';
-import './Layout.css';
+import '../../new_css/app_css/App.css';
 
 const Layout = (props) => {
     return (
         <Fragment>
-            <Toolbar loggedIn={props.loggedIn} />
-            <div className="layout-body">
-                {props.children}
+            <div class="page-content">
+                <Toolbar loggedIn={props.loggedIn} />
+                <div class="layout-body">
+                    <div class="loaded-content">
+                        {props.children}
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <div class="container-fluid">
+                    <div class="footer-left">Polski</div>
+                    <div class="footer-right">Token nie wygaśnie</div>
+                    <div class="footer-center">
+                    <div class="footer-center">
+                        <span class="text-muted">SUPLA 2D Home </span>
+                        <a href="https://www.supla.org" class="brand-nav-link">www.supla.org</a></div>
+                    </div>
+                </div>
             </div>
         </Fragment>
     );
