4af7d28386228c658c4654f1fda6ae47be0bd20f
Katarzyna Strzyż <kstrzyz@student.agh.edu.pl>
2020-12-02 19:11:18 +0100

    Add react-player and About Us page
    Former-commit-id: 3e23e6db31184b98ee38e9cfee50c878df42258d

---
+++ frontend/src/components/About/AboutUs.js
@@ -0,0 +1,12 @@
+import ReactPlayer from "react-player";
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <ReactPlayer style={{marginLeft: "30%", marginTop: "10%"}}url="https://www.youtube.com/watch?v=c9ubC7nBM-A"/>
+        </div>
+    );
+};
+
+export default AboutUs;
