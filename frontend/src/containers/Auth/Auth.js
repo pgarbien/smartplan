44fac7584d9181ae1187848ba5a24a5e3809226e
RetroSuperHero <37156557+RetroSuperHero@users.noreply.github.com>
2020-12-02 18:41:07 +0100

    Changed auth container for refresh token
    Former-commit-id: c41ecdd6199a7d64dc4d447418b4e36b1a15936c

--- frontend/src/containers/Auth/Auth.js
+++ frontend/src/containers/Auth/Auth.js
@@ -1,20 +1,27 @@
 import React, { useEffect, Fragment } from 'react'
 import { useHistory } from "react-router-dom";
+import mAxios from '../../utils/API';
 
 const Auth = (props) => {
     const history = useHistory();
 
-    useEffect(() => saveAuthToken());
+    useEffect(() => saveAuthToken(), []);
 
     const saveAuthToken = () => {
       const query = window.location.search;
       const params = new URLSearchParams(query); 
       const token = params.get('token');
+      const tokenTimeout = params.get('timeout')
 
-      if(token != null) {
-          localStorage.setItem('token', token);
+      if(tokenTimeout) {
+          setInterval(() => {
+            mAxios.get('/auth/refresh')
+              .then(response => localStorage.setItem('token', response.data.token))
+              .catch(() => props.setLoggedIn(false));
+          }, 0.75 * tokenTimeout * 1000);
       }
   
+      localStorage.setItem('token', token);
       props.setLoggedIn(localStorage['token']);
       history.push('/locations');
     }
