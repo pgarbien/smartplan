b5389a99084de738b5e660eb04cc47013b1d03d0
Katarzyna Strzyż <kstrzyz@student.agh.edu.pl>
2020-12-05 20:36:32 +0100

    Add about us video
    Former-commit-id: 963926f781cae6de57fb28c24166c46e265083ab

--- frontend/src/components/About/AboutUs.js
+++ frontend/src/components/About/AboutUs.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 
 const AboutUs = () => {
     return (
         <div>
+            <ReactPlayer url='https://www.youtube.com/watch?v=hXyCk6RbwWA' style={{marginLeft: "35%", marginTop: "10%"}}/>
         </div>
     );
 };
