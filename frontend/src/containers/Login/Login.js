d16391893c7658f14a9117ed670e900c744ebdb0
Katarzyna Strzyż <kstrzyz@student.agh.edu.pl>
2020-12-05 22:55:03 +0100

    Add tutorial video to main page
    Former-commit-id: 36f74a9eba722921815dc3c595176a2b5286c450

--- frontend/src/containers/Login/Login.js
+++ frontend/src/containers/Login/Login.js
@@ -1,13 +1,19 @@
 import React, { useEffect, Fragment } from 'react';
 import '../../new_css/app_css/App.css';
-
+import ReactPlayer from 'react-player';
+import {useTranslation} from 'react-i18next';
 const Login = (props) => {
+    const {t, i18n} = useTranslation('main');
     useEffect(() => { props.setLoggedIn(localStorage['token']) });
   
     return (
         <Fragment>
             <div class="body-container home-page">
-                <div class="main-page-image"></div>  
+                <div class="main-page-image"></div> 
+                <h1 class="video-text">
+                    {t('login.howTo')}
+                </h1>
+                <ReactPlayer class="supla-video" url='https://www.youtube.com/watch?v=nt4ejdr5PRY&feature=youtu.be' /> 
             </div>  
         </Fragment>
     );
